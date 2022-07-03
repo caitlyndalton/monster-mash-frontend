358026441417a5983358990b89d4c38e83c2a2ca
Caitlyn Dalton <cdalton0610@gmail.com>
2022-07-03 12:08:39 -0400

    Added index/create function to frontend home.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,54 @@
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      campaigns: [],
+      newCampaignParams: {},
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexCampaigns();
+  },
+  methods: {
+    indexCampaigns: function () {
+      axios.get("/campaigns").then((response) => {
+        console.log("campaigns index", response);
+        this.campaigns = response.data;
+      });
+    },
+    createCampaign: function () {
+      axios
+        .post("/campaigns", this.newCampaignParams)
+        .then((response) => {
+          console.log("campaigns create", response);
+          this.campaigns.push(response.data);
+          this.newCampaignParams = {};
+        })
+        .catch((error) => {
+          console.log("campaigns create error", error.response);
+        });
+    },
+  },
 };
 </script>
 
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>New Campaign</h1>
+    <div>
+      Name:
+      <input type="text" v-model="newCampaignParams.name" />
+      Description:
+      <input type="text" v-model="newCampaignParams.description" />
+      <button v-on:click="createCampaign()">Create Campaign</button>
+    </div>
+    <h1>All Campaigns</h1>
+    <div v-for="campaign in campaigns" v-bind:key="campaign.id">
+      <h2>{{ campaign.name }}</h2>
+      <p>Description: {{ campaign.description }}</p>
+    </div>
   </div>
 </template>
 
