3854f8d9edf76601dd53262c0c559a176f2d98a2
Caitlyn Dalton <cdalton0610@gmail.com>
2022-07-11 21:40:53 -0400

    Added NPC/creature  vue create function

--- src/views/CreatureView.vue
+++ src/views/CreatureView.vue
@@ -4,10 +4,15 @@ export default {
   data: function () {
     return {
       creatures: [],
+      newCreatureParams: {},
     };
   },
   created: function () {
     this.indexCreatures();
+    // axios.get("https://www.dnd5eapi.co/api/monsters/goblin").then((response) => {
+    //   console.log(response.data);
+    //   this.creatures = response.data;
+    // });
   },
   methods: {
     indexCreatures: function () {
@@ -16,16 +21,40 @@ export default {
         this.creatures = response.data;
       });
     },
+    createCreature: function () {
+      axios
+        .post("/creatures", this.newCreatureParams)
+        .then((response) => {
+          console.log("creature create", response);
+          this.creatures.push(response.data);
+          this.newCreatureParams = {};
+        })
+        .catch((error) => {
+          console.log("creatures create error", error.response);
+        });
+    },
   },
 };
 </script>
 
 <template>
   <div class="creatures-index">
+    <h1>New Creature</h1>
+    <div>
+      Api:
+      <input type="text" v-model="newCreatureParams.api_index" />
+      Type:
+      <input type="text" v-model="newCreatureParams.api_type" />
+      Name:
+      <input type="text" v-model="newCreatureParams.name" />
+      Campaign:
+      <input type="text" v-model="newCreatureParams.campaign_id" />
+      <button v-on:click="createCreature()">Create Creature</button>
+    </div>
     <h1>All Creatures</h1>
     <div v-for="creature in creatures" v-bind:key="creature.id">
       <h2>{{ creature.name }}</h2>
-      <p>Type: {{ creature.api_type }}</p>
+      <p>{{ creature.api_type }}</p>
     </div>
   </div>
 </template>
--- src/views/NpcView.vue
+++ src/views/NpcView.vue
@@ -4,6 +4,7 @@ export default {
   data: function () {
     return {
       npcs: [],
+      newNpcParams: {},
     };
   },
   created: function () {
@@ -16,12 +17,40 @@ export default {
         this.npcs = response.data;
       });
     },
+    createNpc: function () {
+      axios
+        .post("/npc", this.newNpcParams)
+        .then((response) => {
+          console.log("npc create", response);
+          this.npc.push(response.data);
+          this.newNpcParams = {};
+        })
+        .catch((error) => {
+          console.log("npc create error", error.response);
+        });
+    },
   },
 };
 </script>
 
 <template>
   <div class="npcs-index">
+    <h1>New Npc</h1>
+    <div>
+      Name:
+      <input type="text" v-model="newNpcParams.name" />
+      Race:
+      <input type="text" v-model="newNpcParams.race" />
+      Class:
+      <input type="text" v-model="newNpcParams.api_class" />
+      Alignment:
+      <input type="text" v-model="newNpcParams.alignment" />
+      Level:
+      <input type="text" v-model="newNpcParams.level" />
+      Campaign:
+      <input type="text" v-model="newNpcParams.campaign_id" />
+      <button v-on:click="createNpc()">Create Npc</button>
+    </div>
     <h1>All Npcs</h1>
     <div v-for="npc in npcs" v-bind:key="npc.id">
       <h2>{{ npc.name }}</h2>
