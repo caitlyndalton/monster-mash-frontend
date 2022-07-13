f24262ddd4e369660113f34491541439e31b87f8
Caitlyn Dalton <cdalton0610@gmail.com>
2022-07-12 20:26:01 -0400

    Installed basic bootstrap theme with proper routes/pages

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,38 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/campaigns">Campaigns</router-link>
-    |
-    <router-link to="/creatures">Creatures</router-link>
-    |
-    <router-link to="/items">Items</router-link>
-    |
-    <router-link to="/npcs">Npcs</router-link>
-    |
-    <router-link to="/login">Login</router-link>
-    |
-    <router-link to="/signup">Signup</router-link>
+  <div id="app">
+    <!-- Responsive navbar-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container px-lg-5">
+        <a class="navbar-brand" href="/">Monster Mash</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item"><a class="nav-link active" aria-current="page" href="/">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="/campaigns">Campaigns</a></li>
+            <li class="nav-item"><a class="nav-link" href="/creatures">Monsters</a></li>
+            <li class="nav-item"><a class="nav-link" href="/items">Items</a></li>
+            <li class="nav-item"><a class="nav-link" href="/npcs">Npcs</a></li>
+            <li class="nav-item"><a class="nav-link" href="/signup">Signup</a></li>
+            <li class="nav-item"><a class="nav-link" href="/login">Login</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </div>
   <router-view />
+  <footer class="py-5 bg-dark">
+    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
+  </footer>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script>
 export default {
   data: function () {
-    return {
-      message: "Welcome to Monster Mash",
-    };
+    return {};
   },
   created: function () {},
   methods: {},
@@ -12,7 +10,21 @@ export default {
 
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <!-- Header-->
+    <header class="py-5">
+      <div class="container px-lg-5">
+        <div class="p-4 p-lg-5 bg-light rounded-3 text-center">
+          <div class="m-4 m-lg-5">
+            <h1 class="display-5 fw-bold">Welcome to Monster Mash</h1>
+            <p class="fs-4">
+              Bootstrap utility classes are used to create this jumbotron since the old component has been removed from
+              the framework. Why create custom CSS when you can use utilities?
+            </p>
+            <a class="btn btn-primary btn-lg" href="/campaigns">Start a Campaign</a>
+          </div>
+        </div>
+      </div>
+    </header>
   </div>
 </template>
 
